9378d05a12ebbdfc250485f84c84aeaa5acefcc3
Kolby Graham <kolby@kolbyscomputers.ca>
2022-06-10 16:40:41 -0700

    Added TrueNAS Dataset Info Script

---
+++ TrueNAS/Get-DatastoreUsage.ps1
@@ -0,0 +1,45 @@
+#Created by Kolby Graham
+#Requires -Version 7.0
+
+### Settings ###
+$URI = "https://my-truenas-server.domain.com/api/v2.0"
+$token = "truenas api token"
+$DebugPreference = 'Continue'
+$SQLServerURI = "mysqlserver.domain.com"
+$SQLServerDB = "TrueNAS-Historical"
+$SQLDatasetUsageTableName = "DatasetUsage"
+
+
+### Endpoints ###
+$TNDataset = "/pool/dataset"
+function GetDatasetDetails($token){
+    $Headers = @{
+        'Authorization' = "Bearer $token";
+    }
+    Write-Debug "Getting data from TrueNAS"
+    $Response = Invoke-RestMethod -Uri ($URI + $TNDataset) -Method Get -Headers $Headers -UseBasicParsing -SkipCertificateCheck -ContentType "application/json"
+    WriteDetailsToDB($Response);
+}
+function WriteDetailsToDB($DatasetInfo){
+    Write-Debug "Writing data to DB"
+    $Connection = New-Object System.Data.SQLClient.SQLConnection
+    $Connection.ConnectionString = "server='$SQLServerURI';database='$SQLServerDB';trusted_connection=true;"
+    $Connection.Open()
+    $Command = New-Object System.Data.SQLClient.SQLCommand
+    $Command.Connection = $Connection
+    foreach($dataset in $DatasetInfo){
+        $id = $dataset.id
+        Write-Debug "Working on $id"
+        $usedspace = $dataset.used.rawvalue
+        Write-Debug "Used Space: $usedspace"
+        $ts = get-date -Format 'yyyy-MM-dd HH:mm'
+        $insertquery="
+        INSERT INTO $SQLDatasetUsageTableName
+            ([datasetid],[timestamp],[used])
+          VALUES
+            ('$id','$ts','$usedspace')"
+        $Command.CommandText = $insertquery
+        $Command.ExecuteNonQuery()
+    }
+}
+GetDatasetDetails($token);
